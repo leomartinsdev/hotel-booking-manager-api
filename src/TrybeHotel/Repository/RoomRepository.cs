9b54aa924da2f50b5e9cfadadb534a38cadddff2
Leonardo Martins <leonardogustavo@outlook.com>
2024-01-08 16:41:14 -0300

    Implemented the endpoint DELETE /room

--- src/TrybeHotel/Repository/RoomRepository.cs
+++ src/TrybeHotel/Repository/RoomRepository.cs
@@ -1,5 +1,6 @@
 using TrybeHotel.Models;
 using TrybeHotel.Dto;
+using Microsoft.EntityFrameworkCore;
 
 namespace TrybeHotel.Repository
 {
@@ -66,7 +67,10 @@ namespace TrybeHotel.Repository
         // 8. Desenvolva o endpoint DELETE /room/:roomId
         public void DeleteRoom(int RoomId)
         {
-            throw new NotImplementedException();
+            var deletedRoom = _context.Rooms.FirstOrDefault(e => e.RoomId == RoomId);
+
+            _context.Rooms.Remove(deletedRoom);
+            _context.SaveChanges();
         }
     }
 }
