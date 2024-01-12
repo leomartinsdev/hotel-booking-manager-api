335da41dea429e89192e0c38a84262f3a43c224d
Leonardo Martins <leonardogustavo@outlook.com>
2024-01-12 15:02:37 -0300

    Refactor: POST /city

--- src/TrybeHotel/Dto/CityDto.cs
+++ src/TrybeHotel/Dto/CityDto.cs
@@ -2,5 +2,7 @@ namespace TrybeHotel.Dto {
      public class CityDto {
         public int cityId { get; set; }
         public string? name { get; set; }
+
+        public string state {  get; set; }
     }
 }
--- src/TrybeHotel/Repository/CityRepository.cs
+++ src/TrybeHotel/Repository/CityRepository.cs
@@ -14,20 +14,25 @@ namespace TrybeHotel.Repository
         // 4. Refatore o endpoint GET /city
         public IEnumerable<CityDto> GetCities()
         {
-                       IEnumerable<CityDto> Cities = from city in _context.Cities
-                                            select new CityDto() { cityId = city.CityId, name = city.Name };
+            IEnumerable<CityDto> Cities = from city in _context.Cities
+                                          select new CityDto() { cityId = city.CityId, name = city.Name };
             return Cities.ToList();
         }
 
         // 2. Refatore o endpoint POST /city
         public CityDto AddCity(City city)
         {
-                        _context.Cities.Add(city);
+            _context.Cities.Add(city);
             _context.SaveChanges();
 
             var query = from cityElement in _context.Cities
                         where cityElement.Name == city.Name
-                        select new CityDto() { cityId = cityElement.CityId, name = cityElement.Name };
+                        select new CityDto()
+                        {
+                            cityId = cityElement.CityId,
+                            name = cityElement.Name,
+                            state = cityElement.State
+                        };
 
             return query.First();
         }
@@ -35,7 +40,7 @@ namespace TrybeHotel.Repository
         // 3. Desenvolva o endpoint PUT /city
         public CityDto UpdateCity(City city)
         {
-           throw new NotImplementedException();
+            throw new NotImplementedException();
         }
 
     }
